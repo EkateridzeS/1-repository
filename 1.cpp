c1d0a7852fe6196a25e4224cef4aea40c9d59dbe
Zelenova Ekateerina <ezelenova08@yandex.ru>
2024-05-20 01:33:46 +0300

    recursive calculation of the nth Fibonacci number, provided that the sequence starts with (1, 1, ...).

--- 1.cpp
+++ 1.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello, world!" << endl;
+int fib(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+int main(){
+    int n;
+    cout << "n= ";
+    cin >> n;
+
+    int t = fib(n);
+    cout << "fib(" << n << ") = " << t << endl;
+
     return 0;
 }
